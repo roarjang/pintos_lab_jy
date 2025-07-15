ed7f147ebdd1a8aa184776c2dadcffa08aec60c4
Ubuntu <ubuntu@ip-172-31-40-160.ap-northeast-2.compute.internal>
2025-07-15 21:07:18 +0000

    feat: make semaphore waiters priority-aware and pass priority-sema test

--- pintos/threads/synch.c
+++ pintos/threads/synch.c
@@ -67,7 +67,7 @@ void sema_down(struct semaphore *sema)
 	old_level = intr_disable();
 	while (sema->value == 0)
 	{
-		list_push_back(&sema->waiters, &thread_current()->elem);
+		list_insert_ordered(&sema->waiters, &thread_current()->elem, compare_priority_desc, NULL);
 		thread_block();
 	}
 	sema->value--;
@@ -109,12 +109,26 @@ void sema_up(struct semaphore *sema)
 
 	ASSERT(sema != NULL);
 
-	old_level = intr_disable();
+	struct thread *sema_thread;
+
 	if (!list_empty(&sema->waiters))
-		thread_unblock(list_entry(list_pop_front(&sema->waiters),
-															struct thread, elem));
-	sema->value++;
-	intr_set_level(old_level);
+	{
+		old_level = intr_disable();
+		sema->value++;
+		sema_thread = list_entry(list_pop_front(&sema->waiters), struct thread, elem);
+		intr_set_level(old_level);
+
+		thread_unblock(sema_thread);
+
+		if (thread_current()->priority < sema_thread->priority)
+			thread_yield();
+	}
+	else
+	{
+		old_level = intr_disable();
+		sema->value++;
+		intr_set_level(old_level);
+	}
 }
 
 static void sema_test_helper(void *sema_);
